bbe622f711530274399f7550a9d4087f6fd91efb
Andraad3 <joao.andraade25@gmail.com>
2022-11-18 22:57:33 -0300

    vercel config

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const app = express()
 
 routes(app)
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
 
