3e5b643932f007895fc035ebd295871a08e9e4b9
Andraad3 <joao.andraade25@gmail.com>
2022-12-04 02:21:08 -0300

    Alteracao rota Evento

--- api/modules/getEvento.js
+++ api/modules/getEvento.js
@@ -4,9 +4,9 @@ class GetEvento {
     static async getEvento(req, res) {
         console.log('Requisição: ', req);
         try {
-            let { data: evento, error } = await supabase.from('evento').select('*').eq('id', 1)
+            let response = await supabase.from('evento').select('*').eq('id', req.query.id)
 
-            return res.status(200).json(evento)
+            return res.status(200).json(response)
         } catch (error) {
             return res.status(500).json(error.message)
         }
