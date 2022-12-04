e3182866dc2faa6d42d4a3cb379ce5869bee6296
Andraad3 <joao.andraade25@gmail.com>
2022-12-04 02:21:16 -0300

    alteração rota cadastro

--- api/modules/cadastro.js
+++ api/modules/cadastro.js
@@ -2,15 +2,15 @@ const supabase = require("./supabase")
 
 class Cadastro {
     static async cadastro(req, res) {
-        console.log('Requisição: ', req);
+        console.log('Requisição: ', req.body);
         try {
-            const { data: imovel, error } = await supabase
+            const response = await supabase
                 .from('users')
                 .insert([
-                    { some_column: 'someValue', other_column: 'otherValue' },
+                    { nome: req.body.nome, email: req.body.email, senha: req.body.password },
                 ])
 
-            return res.status(200).json({ imovel, error })
+            return res.status(200).json(response)
         } catch (error) {
             return res.status(500).json(error.message)
         }
