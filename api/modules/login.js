c14545dcb7ea0552f19a0b8f7faed48bb39c6046
Andraad3 <joao.andraade25@gmail.com>
2022-12-04 02:20:40 -0300

    alteração rota de login

--- api/modules/login.js
+++ api/modules/login.js
@@ -4,14 +4,14 @@ class Login {
     static async login(req, res) {
         console.log('Requisição: ', req);
         try {
-            let { data: Users, error } = await supabase
+            const response = await supabase
                 .from('users')
                 .select("*")
-                .eq('email', email)
-                .eq('password', senha)
+                .eq('email', req.body.email)
+                .eq('senha', req.body.password)
 
 
-            return res.status(200).json({ Users, error })
+            return res.status(200).json(response)
         } catch (error) {
             return res.status(500).json(error.message)
         }
